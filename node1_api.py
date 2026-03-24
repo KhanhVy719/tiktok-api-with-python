63122dbcef13a782738bd56a9e3ac2cf65e36eaa
KhanhWiee <khanhwiee@gmail.com>
2026-03-25 03:10:37 +0700

    fix: add CORS headers for cross-origin API access

--- node1_api.py
+++ node1_api.py
@@ -31,6 +31,13 @@ PORT = 8888
 
 app = Flask(__name__)
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
+
 
 def load_meta():
     if os.path.exists(META_FILE):
